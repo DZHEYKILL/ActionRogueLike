fab8a8b603507cc968c66197df86dd3cb4b0fd67
DZHAMBOLAT KASAEV <william.ddd7@gmail.com>
2025-02-04 13:26:10 +0300

    AI Movement/Attack 42

--- Source/ActionRogueLike/Public/AI/SBTService_CheckAttackRange.h
+++ Source/ActionRogueLike/Public/AI/SBTService_CheckAttackRange.h
@@ -13,5 +13,10 @@ UCLASS()
 class ACTIONROGUELIKE_API USBTService_CheckAttackRange : public UBTService
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	FBlackboardKeySelector AttackRangeKey;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 };
